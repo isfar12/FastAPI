a069eb8273b92700925f797ac6c0bda900d9ca54
Jubayer Isfar <isfarjubayer@gmail.com>
2026-01-13 18:58:50 +0600

    fixed new query

--- Youtube Tuts/5. User Authentication System/app.py
+++ Youtube Tuts/5. User Authentication System/app.py
@@ -40,15 +40,22 @@ async def register(user: Register, db: Session = Depends(get_db)):
 async def login(user: Login, db: Session = Depends(get_db)):
     query = select(User).where(User.username == user.username)
     # returns the list of matched user objects
-    existing_user = db.execute(query).first()
+    existing_user = db.query(User).filter(User.username == user.username).first()
     if not existing_user:
         raise HTTPException(
             status_code=400, detail="Invalid Username or Password")
 
     is_valid = password_context.verify(
-        user.password, existing_user[0].password)
+        user.password, existing_user.password)
     if not is_valid:
         raise HTTPException(
             status_code=400, detail="Invalid Username or Password")
 
-    return {"message": "Login successful!", "username": existing_user[0].username}
+    return {"message": "Login successful!", "username": existing_user.username}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
